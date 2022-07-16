f5272c2df3373f6682cf8736428fdfc496892907
Adam Ozeri <80855756+adamozeri@users.noreply.github.com>
2022-07-16 13:47:35 +0300

    throw

--- Assets/player/scripts/pick_up.cs
+++ Assets/player/scripts/pick_up.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class pick_up : MonoBehaviour
 {
-    private bool isUp;
-    public Vector2 position;
+    private Rigidbody2D rb2;
+    public bool isUp;
+    public Vector3 position = new Vector3(0.2f,3,0);
+    public Collider2D pickupCol;
+    public GameObject map;
+    public GameObject picked;
     
     // Start is called before the first frame update
     void Start()
     {
+        rb2 = GetComponent<Rigidbody2D>();
         isUp = false;
-        position = new Vector2(0,3);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isUp)
+        {
+            picked.transform.position = this.transform.position + position;
+        }
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("F");
+        }
+
     }
 
     private void FixedUpdate()
@@ -25,15 +37,26 @@ public class pick_up : MonoBehaviour
 
     }
 
-    private void OnTriggerEnter2D(Collider2D collider)
+    private void OnTriggerStay2D(Collider2D other)
     {
-        Debug.Log(collider);
-        collider.transform.SetParent(this.transform);
-        collider.transform.position = position;
-    }
-
-     private void OnTriggerExit2D(Collider2D collider)
-    {
-        
+        Debug.Log("touching" + Input.GetKeyDown(KeyCode.F));
+        if(Input.GetKeyDown(KeyCode.F) && pickupCol.IsTouching(other) && !isUp)
+        {
+            Debug.Log("hello");
+            picked = other.gameObject;
+            other.attachedRigidbody.freezeRotation = true;
+            other.gameObject.transform.SetParent(this.gameObject.transform);
+            other.attachedRigidbody.gravityScale = 0;
+            other.transform.position = other.transform.position + position;
+            isUp = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.F) && isUp)
+        {
+            Debug.Log("bye");
+            picked = null;
+            other.gameObject.transform.SetParent(map.transform);
+            isUp = false;
+            other.attachedRigidbody.gravityScale = 10;
+        }
     }
 }
