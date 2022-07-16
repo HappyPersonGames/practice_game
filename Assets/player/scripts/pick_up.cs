a645b8c9c3bbcc8102b40ec9bef6e0dca22755b4
OdedGinat2 <odedginat@gmail.com>
2022-07-16 21:49:36 +0300

    movment_fix

--- Assets/player/scripts/pick_up.cs
+++ Assets/player/scripts/pick_up.cs
@@ -10,6 +10,8 @@ public class pick_up : MonoBehaviour
     public Collider2D pickupCol;
     public GameObject map;
     public GameObject picked;
+
+    public bool pressed_key = false;
     
     // Start is called before the first frame update
     void Start()
@@ -21,42 +23,51 @@ public class pick_up : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            pressed_key = true;
+        }
+        if(Input.GetKeyUp(KeyCode.F))
+        {
+            pressed_key = false;
+        }
         if(isUp)
         {
             picked.transform.position = this.transform.position + position;
-        }
-        if(Input.GetKeyDown(KeyCode.F))
-        {
-            Debug.Log("F");
         }
 
     }
 
     private void FixedUpdate()
     {
-
+        // pressed_key = false;
     }
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        Debug.Log("touching" + Input.GetKeyDown(KeyCode.F));
-        if(Input.GetKeyDown(KeyCode.F) && pickupCol.IsTouching(other) && !isUp)
+        // Debug.Log("touching " + Input.GetKeyDown(KeyCode.F) + other.name);
+        if(pressed_key && pickupCol.IsTouching(other) && !isUp)
         {
-            Debug.Log("hello");
+            // Debug.Log("hello");
             picked = other.gameObject;
             other.attachedRigidbody.freezeRotation = true;
             other.gameObject.transform.SetParent(this.gameObject.transform);
             other.attachedRigidbody.gravityScale = 0;
             other.transform.position = other.transform.position + position;
             isUp = true;
+            pressed_key = false;
+            return;
         }
-        else if(Input.GetKeyDown(KeyCode.F) && isUp)
+        else if(pressed_key && pickupCol.IsTouching(other) && isUp)
         {
-            Debug.Log("bye");
-            picked = null;
-            other.gameObject.transform.SetParent(map.transform);
-            isUp = false;
+            // Debug.Log("bye");
+            picked.gameObject.transform.SetParent(map.transform);
             other.attachedRigidbody.gravityScale = 10;
+            other.attachedRigidbody.freezeRotation = false;
+            other.attachedRigidbody.AddForce(new Vector2(1000, 1000));
+            isUp = false;
+            picked = null;
+            pressed_key = false;
         }
     }
 }
